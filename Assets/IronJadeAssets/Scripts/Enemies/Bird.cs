05f377db1d2c1a93b791f8bcd1e0c36030b0f5fd
jose <josmontenegro2012@gmail.com>
2018-03-21 22:11:09 -0400

    Added the base Target class. Made the Bird class

---
+++ Assets/IronJadeAssets/Scripts/Enemies/Bird.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Bird enemy
+/// </summary>
+public class Bird : Target {
+
+    /// <summary>
+    /// Bird's movement speed
+    /// </summary>
+    public float speed = 2f;
+
+    /// <summary>
+    /// Current vertical direction of flight
+    /// </summary>
+    private float dir;
+
+	/// <summary>
+    /// Initialization of private variables
+    /// </summary>
+	void Start () {
+        dir = 1f;
+	}
+	
+	/// <summary>
+    /// Update movement
+    /// </summary>
+	void FixedUpdate () {
+        transform.Translate(Vector3.up * speed * dir * Time.fixedDeltaTime);
+	}
+
+    /// <summary>
+    /// Implementation of parent GetShot
+    /// </summary>
+    public override void GetShot(){
+        dir = -1f;
+    }
+}
